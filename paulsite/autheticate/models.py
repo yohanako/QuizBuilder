d43bb095aa24fa3b5658533d8db194f7be5a1036
Paul <78459874+yohanako@users.noreply.github.com>
2021-02-28 23:56:37 -0800

    Add files via upload

---
+++ paulsite/autheticate/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Questions(models.Model):
+    question = models.CharField(max_length=100)
+
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
